02364b625be6a172d0c03f502b96c0b5c22b1c8d
Spenser Haddad <spenser.haddad@gmail.com>
2026-04-24 12:20:05 -0400

    Properly show log output in docker image

--- docker-start.sh
+++ docker-start.sh
@@ -6,8 +6,8 @@ else
   CRON_SCHEDULE="${FDL_CRON_SCHEDULE}"
 fi
 
-echo "${CRON_SCHEDULE} uv run --directory /factorio-downloader python -m factorio_downloader --version=latest --build=expansion --outdir /downloaded --quiet --logfile /downloaded/factorio-dl.log" >> /etc/cron.d/factorio-dl 
+echo "${CRON_SCHEDULE} uv run --directory /factorio-downloader python -m factorio_downloader --version=latest --build=expansion --outdir /downloaded --logfile /downloaded/factorio-dl.log" >> /etc/cron.d/factorio-dl 
 chmod 0644 /etc/cron.d/factorio-dl && crontab /etc/cron.d/factorio-dl
 mkdir -p /downloaded
 
-crond -S
+crond -L /crond.log && tail -f /crond.log
